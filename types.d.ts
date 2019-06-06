276dac0b23fe60da7441f671ef8dc749952e6026
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2019-06-06 19:19:20 +0300

    Update declaration

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,9 @@
-type ScriptType
+export type ScriptType
     = 'LEGACY'
     | 'P2SH'
     | 'BECH32';
 
-
-type BaseUtxo = {
+export type BaseUtxo = {
     txId: string;
     vout: number;
     value: number;
@@ -12,21 +11,18 @@ type BaseUtxo = {
     script?: string | { length: number; };
 };
 
-
-type BaseTarget = {
+export type BaseTarget = {
     address: string;
     value?: number;
     type?: ScriptType;
     script?: string | { length: number; };
 };
 
-
-type CoinSelectResult<I extends BaseUtxo, O extends BaseTarget> = {
+export type CoinSelectResult<I extends BaseUtxo, O extends BaseTarget> = {
     inputs?: I[];
     outputs?: O[];
     fee: number;
 };
-
 
 declare function coinSelect<
     I extends BaseUtxo = BaseUtxo,
@@ -36,7 +32,6 @@ declare function coinSelect<
     targets: O[],
     feeRate: number
 ): CoinSelectResult<I, O>;
-
 
 declare module 'coinselect' {
     export = coinSelect;
